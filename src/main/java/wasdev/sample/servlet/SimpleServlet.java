a7385b5238006e4e6928a87ec917d1707d7bf1da
wubiao <wubiao@ADMINIB-TQR007K>
2017-02-15 14:17:35 +0800

    MySQL services Updated

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -8,18 +8,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//STEP 1. Import required packages
+import java.sql.*;
+
 /**
  * Servlet implementation class SimpleServlet
  */
 @WebServlet("/SimpleServlet")
 public class SimpleServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    
+    //JBDC driver name and database URL (Database credentials)
+    static final String JDBC_URL = "jdbc:mysql://us-cdbr-iron-east-04.cleardb.net/ad_c044be4d4a601c9?user=beb0c072c5678f&password=879adaa7";
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	StringBuilder msgPage = new StringBuilder();
+    	msgPage.append("<br>STEP 1. Import required packages");
+    	
         response.setContentType("text/html");
         response.getWriter().print("Hello World!");
     }
